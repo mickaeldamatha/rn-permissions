d9e85b406ef1a068fe21da8a0a48923b42599f23
Mickael Da matha sant'anna <113256836+mickael-damathasantanna@users.noreply.github.com>
2022-12-12 07:04:49 +0100

    Project Init :rocket:

--- lib/Spacer.d.ts
+++
@@ -1,5 +0,0 @@
-export default function Spacer(props: {
-    space: number;
-    flex: number;
-    row: boolean;
-}): JSX.Element;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,12 @@
-import FlexView from "./FlexView";
-import Spacer from "./Spacer";
-import TextField from "./TextField";
-export { TextField, FlexView, Spacer };
+import { ReactNode } from "react";
+import { Notification, NotificationResponse } from "expo-notifications";
+export default function PermissionProvider(props: {
+    children: ReactNode;
+    camera?: boolean;
+    notifications?: boolean;
+    microphone?: boolean;
+    handleNotificationResponse: (response: NotificationResponse) => void;
+    handleNotification: (notification: Notification) => void;
+    handlePushToken: (token: string) => void;
+    handleError: (message: string) => void;
+}): JSX.Element;
