5edda8a53fbbafc820d203c0653da940660e2cb2
Mickael Da matha sant'anna <113256836+mickael-damathasantanna@users.noreply.github.com>
2022-12-12 07:03:44 +0100

    Project Init :rocket:

---
+++ src/index.tsx
@@ -0,0 +1,91 @@
+import { isDevice } from "expo-device";
+import { useEffect, ReactNode } from "react";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+import { Camera } from "expo-camera";
+import { useRef } from "react";
+import {
+  AndroidNotificationPriority,
+  Notification,
+  NotificationResponse,
+} from "expo-notifications";
+
+export default function PermissionProvider(props: {
+  children: ReactNode;
+  camera?: boolean;
+  notifications?: boolean;
+  microphone?: boolean;
+  handleNotificationResponse: (response: NotificationResponse) => void;
+  handleNotification: (notification: Notification) => void;
+  handlePushToken: (token: string) => void;
+  handleError: (message: string) => void;
+}) {
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+
+  useEffect(() => {
+    
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+        priority: AndroidNotificationPriority.DEFAULT,
+      }),
+      handleError: (pushId, error) => {},
+      handleSuccess: (notificationId) => {},
+    });
+
+    (async () => {
+      if (isDevice && props.notifications) {
+        const { status: existingStatus } =
+          await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+
+          finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+          props.handleError("Unable to get token.");
+          return;
+        }
+        const token = await Notifications.getExpoPushTokenAsync();
+
+        props.handlePushToken(token.data);
+      } else {
+        props.handleError("Not available on simulator device.");
+      }
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+
+      props.camera && (await Camera.requestCameraPermissionsAsync());
+      props.microphone && (await Camera.requestMicrophonePermissionsAsync());
+
+      notificationListener.current =
+        Notifications.addNotificationReceivedListener((notification) => {
+          props.handleNotification(notification);
+        });
+
+      responseListener.current =
+        Notifications.addNotificationResponseReceivedListener((response) => {
+          props.handleNotificationResponse(response);
+        });
+
+      return () => {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+        Notifications.removeNotificationSubscription(responseListener.current);
+      };
+    })();
+  }, []);
+  return <>{props.children}</>;
+}
